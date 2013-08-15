7ab64fe390376e33ba23ef3516dc11852e41d564
linkenpeng <collin_linken@qq.com>
2013-08-15 13:47:03 +0800

    add curl

---
+++ curlTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+import pycurl
+import StringIO
+
+print pycurl.version_info()
+
+html = StringIO.StringIO()
+url = r'https://mail.qq.com/cgi-bin/loginpage'
+c = pycurl.Curl()
+c.setopt(pycurl.URL,url)
+c.setopt(pycurl.SSL_VERIFYHOST, False)
+c.setopt(pycurl.SSL_VERIFYPEER,False)
+c.setopt(pycurl.WRITEFUNCTION, html.write)
+c.setopt(pycurl.FOLLOWLOCATION, 1)
+c.perform()
+print c.getinfo(pycurl.HTTP_CODE), c.getinfo(pycurl.EFFECTIVE_URL)
+print html.getvalue()
+
