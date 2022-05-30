689e46e41813171edb6ce6203e22ef0fc88b381d
peter <peter.peng@watsons.com.cn>
2022-05-30 23:10:58 +0800

    add mayavi

--- src/simple/画三维图.py
+++ src/simple/画三维图.py
@@ -6,29 +6,6 @@ from mayavi import mlab
 x,y=np.ogrid [-2:2:160j,-2:2:160j]
 z=abs(x)*np.exp(-x**2-(y/.75)**2)
 pl=mlab.surf(x,y,z,warp_scale=2)
-mlab.axes(xlabel='x',ylabel='y',zlabel='z')
+mlab.axes(xlabel='x', ylabel='y', zlabel='z')
 mlab.outline(pl)
 mlab.show()
-
-            
-            
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
