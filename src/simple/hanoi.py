b4b09f64b872444eaf329a326b1c0f88f686c8b8
linkenpeng <collin_linken@qq.com>
2023-09-16 09:00:15 +0800

    pygame hello

--- src/simple/hanoi.py
+++ src/simple/hanoi.py
@@ -1,15 +1,15 @@
 # 汉诺塔
-count = 0
+count = 1
 def hanoi(n, src, dst, mid):
     global count
     if n == 1:
-        print("{}:{}->{}".format(1, src, dst))
+        print("step:{} {}:{}->{}".format(count, 1, src, dst))
         count += 1
     else:
         hanoi(n-1, src, mid, dst)
-        print("{}:{}->{}".format(n, src, dst))
+        print("step:{} {}:{}->{}".format(count, n, src, dst))
         count += 1
         hanoi(n-1, mid, dst, src)
 
-hanoi(3, "A", "C", "B")
+hanoi(8, "A", "C", "B")
 print(count)
