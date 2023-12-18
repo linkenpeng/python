f455a74b6c96b6cf6ef94b264978b1a8ecb37cae
linkenpeng <collin_linken@qq.com>
2023-12-18 22:48:11 +0800

    add plot

--- src/library/matplot/plot.py
+++ src/library/matplot/plot.py
@@ -5,9 +5,49 @@ Created on Sun Nov 26 21:40:03 2023
 
 @author: pengzhenxian
 """
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([3, 1, 4, 5, 2])
-plt.ylabel("grade")
-plt.show()
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+def draw():
+    a = np.arange(0.0, 5.0, 0.02)
+    plt.subplot(211)
+    plt.plot(a, f(a))
+    plt.subplot(2,1,2)
+    plt.plot(a, np.cos(2*np.pi*t), 'r--')
+    plt.show()
+    
+    
+def d1():
+    plt.plot([0,2,4,6,8], [3, 1, 4, 5, 2])
+    plt.ylabel("grade")
+    # 行,列,选择区域
+    plt.subplot(3,2,4)
+    # 前两个x轴 ， 后两个y轴
+    plt.axis([-1,10,0,6])
+    # plt.savefig("test", dpi=600)
+    plt.show()
+  
+def multi():    
+  a = np.arange(10)
+  plt.plot(a,a*1.5,'go-', a,a*2.5,'rx', a,a*3.5,'*', a,a*4.5,'b-.')
+  plt.show()
+      
+multi()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
