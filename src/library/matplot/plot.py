5552cd3b29beb8366aaa39dd8eec96980a4f56e3
linkenpeng <collin_linken@qq.com>
2023-12-19 22:23:02 +0800

    plot

--- src/library/matplot/plot.py
+++ src/library/matplot/plot.py
@@ -7,6 +7,7 @@ Created on Sun Nov 26 21:40:03 2023
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 
 def f(t):
@@ -31,12 +32,19 @@ def d1():
     # plt.savefig("test", dpi=600)
     plt.show()
   
-def multi():    
-  a = np.arange(10)
-  plt.plot(a,a*1.5,'go-', a,a*2.5,'rx', a,a*3.5,'*', a,a*4.5,'b-.')
-  plt.show()
+def multi():
+    a = np.arange(10)
+    plt.plot(a,a*1.5,'go-', a,a*2.5,'rx', a,a*3.5,'*', a,a*4.5,'b-.')
+    plt.show()
+  
+def chinese():
+    matplotlib.rcParams['font.family'] = 'SimHei'
+    plt.plot([3, 1, 4, 5, 2])
+    plt.ylabel("数轴（值）")
+    # plt.savefig("test", dpi=600)
+    plt.show()
       
-multi()
+chinese()
 
 
 
