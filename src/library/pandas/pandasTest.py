5208bdc2c620831413b31c0603f6a692ab79d0f8
linkenpeng <collin_linken@qq.com>
2023-12-26 22:43:34 +0800

    add pandas

--- src/library/pandas/pandasTest.py
+++ src/library/pandas/pandasTest.py
@@ -9,33 +9,38 @@ Created on Tue Dec 26 10:52:30 2023
 import pandas as pd
 import numpy as np
 
-def createSeries():
-
-    a = pd.Series([9, 8, 7, 6], index=['a','b','c','d'])
-    #print(a)
+class SeriesOperate(object):
+    def createSeries(self):
     
-    b = pd.Series(25, index=['a','b','c','d'])
-    b.name = 'Series对象'
-    b.index.name = 'Index'
-    print(b)
-    print(b[0])
-    print(b['a'])
-    print(b[:3])
-    print(b.get('f', 100))
-    
-    c = pd.Series({'a':9,'b':8})
-    #print(c)
-    
-    d = pd.Series({'a':9,'b':8} , index=['a','b','c','d'])
-    #print(d)
-    
-    print(c + d)
-    
-    e = pd.Series(np.arange(5), index=np.arange(9,4,-1))
-    #print(e)
-    
-    #print(e.index)
-    #print(e.values)
+        a = pd.Series([9, 8, 7, 6], index=['a','b','c','d'])
+        #print(a)
+        
+        b = pd.Series(25, index=['a','b','c','d'])
+        b.name = 'Series对象'
+        b.index.name = 'Index'
+        print(b)
+        print(b[0])
+        print(b['a'])
+        print(b[:3])
+        print(b.get('f', 100))
+        
+        c = pd.Series({'a':9,'b':8})
+        #print(c)
+        
+        d = pd.Series({'a':9,'b':8} , index=['a','b','c','d'])
+        #print(d)
+        
+        print(c + d)
+        
+        e = pd.Series(np.arange(5), index=np.arange(9,4,-1))
+        #print(e)
+        
+        #print(e.index)
+        #print(e.values)
+    def sort(self):
+        a = pd.Series([9, 8, 7, 6], index=['a','b','c','d'])
+        
+        
 
 class DataFrameOperate(object):
     
@@ -45,7 +50,7 @@ class DataFrameOperate(object):
         print('Created a DataFrameOperate instance for', nm)
         print(50*'-')
     
-    def createDataFrame(self, method = 1):
+    def create(self, method = 1):
         if(method == 1):
             return pd.DataFrame(np.arange(15).reshape(3, 5))
         elif(method == 2):
@@ -65,8 +70,8 @@ class DataFrameOperate(object):
             #print(d.values)
             return d
         
-    def operateDataFrame(self):
-       d = self.createDataFrame(5)
+    def operate(self):
+       d = self.create(5)
        newc = d.columns.insert(2, 'add')
        newd = d.reindex(columns=newc, fill_value=200)
        
@@ -83,7 +88,7 @@ class DataFrameOperate(object):
        print(f.drop('b'))
        print(f.drop('one', axis=1))
        
-    def calDataFrame(self):
+    def cal(self):
         a = pd.DataFrame(np.arange(12).reshape(3, 4))
         b = pd.DataFrame(np.arange(20).reshape(4, 5))
         c = pd.Series(np.arange(5))
@@ -105,11 +110,48 @@ class DataFrameOperate(object):
         #print(a -- d)
         print(a > c)
         print(c > 0)
-
-
+        
+    def sort(self):
+        d = self.create(5)
+        
+        print(d)
+        print(d.sort_index())
+        print(d.sort_index(ascending=False))
+        print(d.sort_index(axis=1, ascending=False))
+        
+        b = pd.DataFrame(np.arange(20).reshape(4, 5))
+        c = d + b
+        #print(d)
+        #print(d.sort_values(0, ascending=False))
+        #print(d.sort_values('a', axis=1, ascending=False))
+        #print(c.sort_values(1, ascending=False))
+        
+    def analysis(self):
+        d = self.create(5)
+        #print(d)
+        #print(d.describe())
+        
+        # 滚动计算
+        #print(d.rolling(1).sum())
+        
+        '''
+        print(d.cumsum())
+        print(d.cumprod())
+        print(d.cummin())
+        print(d.cummax())
+        '''
+        
+        # 计算相关信息
+        hprice = pd.Series([3.04, 22.93, 12.75, 22.6, 12.33], 
+                           index=['2008','2009','2010','2011','2012'])
+        m2 = pd.Series([8.18, 18.38, 9.13, 7.82, 6.99], 
+                           index=['2008','2009','2010','2011','2012'])
+        print(hprice)
+        print(m2)
+        print(hprice.corr(m2))
     
 dfo = DataFrameOperate('Myron')
-dfo.calDataFrame()
+dfo.analysis()
 
 
 
