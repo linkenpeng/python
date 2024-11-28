03cc71c26d7d170b4fb753d29061b46170c257a5
peter.peng <peter.peng@watsons.com.cn>
2024-11-28 18:29:02 +0800

    update curlTest

--- src/net/curlTest.py
+++ src/net/curlTest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #-*- coding:utf8 -*-
+import json
 import sys
 sys.path.append('../')
 import pycurl
@@ -7,13 +8,37 @@ import io
 import time
 from util.TimeUtil import TimeUtil
 
-def get_by_curl(url):
-    print(pycurl.version_info())
+def get_config():
+    config = {}
+    with open('../test_data/ra_config.txt') as file:
+        for line in file:
+            kv = line.split('=')
+            config[kv[0]] = kv[1].strip('\n')
+    return config
+
+def get_test_data():
+    with open('../test_data/ra_test_data.txt') as f:
+        post_data = f.read()
+    print(f'post_data: {post_data}')
+    json_data = json.dumps(post_data)
+    print(f'json_data: {json_data}')
+    return json_data
+
+def get_by_curl(post_json, url, config):
+    # print(pycurl.version_info())
     buffer = io.BytesIO()
     c = pycurl.Curl()
+    headers = [
+        'Content-Type: application/json',
+        'accessKey: ' + config['accessKey']
+    ]
+    # 设置HTTP头部
+    # c.setopt(c.HTTPHEADER, headers)
+    c.setopt(c.HTTPHEADER, headers)
     c.setopt(c.URL, url)
-    # c.setopt(c.TIMEOUT, 1)
-    c.setopt(c.TIMEOUT_MS, 200)
+    c.setopt(c.TIMEOUT, 1)
+    # c.setopt(c.TIMEOUT_MS, 200)
+    c.setopt(c.POSTFIELDS, post_json)
     c.setopt(c.WRITEDATA, buffer)
     try:
         c.perform()
@@ -24,16 +49,14 @@ def get_by_curl(url):
     # 打印响应内容
     print(buffer.getvalue().decode('utf-8'))
 
-if __name__ == '__main__':
-    start = time.perf_counter()
-    get_by_curl('http://localhost:8085/blog/1')
-    end = time.perf_counter()
-    cost_time = TimeUtil.millisecond(start, end)
-    print(f'send_req time: {cost_time}ms')
+def request():
+    config = get_config()
+    url = config['url']
+    post_data = get_test_data()
+    get_by_curl(post_data, url, config)
 
-def get_qq():
+def get_ssl_data(url = 'https://mail.qq.com/cgi-bin/loginpage'):
     html = io.BytesIO()
-    url = 'https://mail.qq.com/cgi-bin/loginpage'
     c = pycurl.Curl()
     c.setopt(pycurl.URL,url)
     c.setopt(pycurl.SSL_VERIFYHOST, False)
@@ -43,4 +66,13 @@ def get_qq():
     c.perform()
     print(c.getinfo(pycurl.HTTP_CODE), c.getinfo(pycurl.EFFECTIVE_URL))
     print(html.getvalue())
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    request()
+    end = time.perf_counter()
+    cost_time = TimeUtil.millisecond(start, end)
+    print(f'send_req time: {cost_time}ms')
+
+
 
