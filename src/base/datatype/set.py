463ea1837e4dbe2876329df19944f3778b9ddf53
peter.peng <peter.peng@watsons.com.cn>
2025-09-23 16:41:10 +0800

    update

--- src/base/datatype/set.py
+++ src/base/datatype/set.py
@@ -1,3 +1,46 @@
+def init():
+    s = {} # 空集合
+    s = {'5元', '10元', '20元'} # 定义集合
+    s = set() # 空集合
+    s = set([1,2,3,4,5]) # {1, 2, 3, 4, 5}（使用列表定义）
+    s = {1, True, 'a'}
+    s = {1, 1, 1} # {1}（去重）
+    type(s) # set（类型检测）
+
+    s = {'a', 'b', 'c'}
+
+    # 判断是否有某个元素
+    'a' in s # True
+
+    # 添加元素
+    s.add(2) # {2, 'a', 'b', 'c'}
+    s.update([1,3,4]) # {1, 2, 3, 4, 'a', 'b', 'c'}
+
+    # 删除和清空元素
+    s.remove('a') # {'b', 'c'}（删除不存在的会报错）
+    s.discard('3') # 删除一个元素，无则忽略，不报错
+    s.clear() # set()（清空）
+
+    s1 = {1,2,3}
+    s2 = {2,3,4}
+
+    s1 & s2 # {2, 3}（交集）
+    s1.intersection(s2) # {2, 3}（交集）
+    s1.intersection_update(s2) # {2, 3}（交集，会覆盖s1）
+
+    s1 | s2  # {1, 2, 3, 4}（并集）
+    s1.union(s2) # {1, 2, 3, 4}（并集）
+
+    s1.difference(s2) # {1}（差集）
+    s1.difference_update(s2) # {1}（差集，会覆盖s1）
+
+    s1.symmetric_difference(s2) # {1, 4}（交集之外）
+
+    s1.isdisjoint(s2) # False（是否没有交集）
+    s1.issubset(s2) # False （s1是否是s2的子集）
+    s1.issuperset(s2) # False（s1是否是s2的超集，即s1是否包含s2中的所有元素）
+
+
 def test1():
     s = set('cheeseshop')
     print(s)
--- src/base/datatype/tuple.py
+++ src/base/datatype/tuple.py
@@ -3,22 +3,38 @@
 def func():
     return 1, 2
 
-# 元组一旦创建就不可修改
-creature = "cat", "dog", "tiger", "human"
+def init():
+    x = (1,2,3,4,5)
+    a, *b = x # a占第一个，剩余的组成列表全给b
+    # a -> 1
+    # b -> [2, 3, 4, 5]
+    # a, b -> (1, [2, 3, 4, 5])
 
-print(creature)
+    a, *b, c = x # a占第一个，c占最后一个，剩余的组成列表全给b
+    # a -> 1
+    # b -> [2, 3, 4]
+    # c -> 5
+    # a, b, c -> (1, [2, 3, 4], 5)
+def test():
+    # 元组一旦创建就不可修改
+    creature = "cat", "dog", "tiger", "human"
 
-color = (0x001100, "blue", creature)
-print(color)
+    print(creature)
 
-print(creature[::-1])
-print(color[-1][2])
+    color = (0x001100, "blue", creature)
+    print(color)
 
-ls = [1, 2, 3]
-it = tuple(ls)
-print(it)
+    print(creature[::-1])
+    print(color[-1][2])
 
-ls2 = list(creature)
-print(ls2)
-it2 = tuple(ls2)
-print(it2)
+    ls = [1, 2, 3]
+    it = tuple(ls)
+    print(it)
+
+    ls2 = list(creature)
+    print(ls2)
+    it2 = tuple(ls2)
+    print(it2)
+
+if __name__ == '__main__':
+    init()
